405a57bab005ccb1bfa921c402c9b4656c5dc4d6
陈斌 <8240926+justin220@user.noreply.gitee.com>
2022-07-23 14:10:31 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 引入公共样式
+import './styles/common.css'
+
+// 引入element-ui
+import 'element-ui/lib/theme-chalk/index.css'
+import Element from 'element-ui'
+Vue.use(Element, { size: 'small', zIndex: 3000 })
+
 new Vue({
   router,
   render: h => h(App)
