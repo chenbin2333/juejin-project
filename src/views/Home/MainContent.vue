bc476ca18781e46f7077817ba30e9ec42c6852b3
Ai ZiTing <1611263796@qq.com>
2022-08-04 21:23:25 +0800

    二级标签-8月4日

--- src/views/Home/MainContent.vue
+++ src/views/Home/MainContent.vue
@@ -2,7 +2,25 @@
   <main class="container">
     <div class="view">
       <!-- 内容导航 -->
-      <nav class="view-nav" :class="{'top':top}"></nav>
+      <nav class="view-nav" :class="{'top':top}">
+      <div class="level-2">
+       <div class="left">
+                    <a href="#" style="color: #1e80ff; padding-left: 0;">综合</a>
+                    <a href="#">关注</a>
+                    <a href="#">后端</a>
+                    <a href="#">前端</a>
+                    <a href="#">Android</a>
+                    <a href="#">iOS</a>
+                    <a href="#">人工智能</a>
+                    <a href="#">开发工具</a>
+                    <a href="#">代码人生</a>
+                    <a href="#">阅读</a>
+                </div>
+                <div class="right">
+                    <a href="#" class="label">标签管理</a>
+                </div>
+                </div>
+      </nav>
       <div class="main-content">
         <!-- 左 -->
         <MainContentLeft></MainContentLeft>
@@ -47,7 +65,7 @@ export default {
   height: 3.833rem;
   z-index: 100;
   box-shadow: 0 1px 2px 0 rgb(0 0 0 / 5%);
-  background-color: #2c3e50;
+  background-color: #fff;
   transition: all .2s;
   transform: translateZ(0);
 }
@@ -55,6 +73,54 @@ export default {
 .top {
   transform: translate3d(0, -5rem, 0);
 }
+.level-2 {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-width: min-content;
+    max-width: 960px;
+    margin: 0 auto;
+
+
+    font-size: 13.92px;
+    line-height: 46px;
+}
+
+.level-2 .right {
+    display: flex;
+    justify-content: end;
+}
+
+.level-2 .right .label {
+    padding-right: 0;
+}
+
+.level-2 a {
+    color: #71777c;
+    padding: 0 12px;
+}
+
+.level-2 a:hover {
+    color: #1e80ff;
+}
+.main {
+    margin: 0 auto;
+    margin-top: 116px;
+    display: flex;
+    justify-content: center;
+}
+.left {
+    width: 700px;
+    background: #fff;
+    
+
+
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 5%);
+    border-radius: 2px;
+}
+  a{text-decoration: none;}
+
+
 .main-content {
   /*height: 5000px;*/
 }
